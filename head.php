ee2ae1dccf8f7b5a48ebfa19a910b291bdca559d
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-06-25 14:01:23 -0400

    Change index to php file and add head php file

---
+++ head.php
@@ -0,0 +1,10 @@
+<head>
+	<meta charset='utf-8'>
+	<meta name='viewport' content='width=device-width, initial-scale=1'>
+
+	<title>PSSST (CSS)</title>
+	<meta name='description' content='PSSST (CSS)'>
+	<meta property='og:image' content='A CSS methodology so great, it needed a name.'>
+
+	<link rel='stylesheet' href='styles/site.css'>
+</head>
--- index.php
+++ index.php
@@ -2,16 +2,7 @@
 
 <html lang='en' class='special-magic no-js'>
 
-	<head>
-		<meta charset='utf-8'>
-		<meta name='viewport' content='width=device-width, initial-scale=1'>
-
-		<title>PSSST (CSS)</title>
-		<meta name='description' content='PSSST (CSS)'>
-		<meta property='og:image' content='A CSS methodology so great, it needed a name.'>
-
-		<link rel='stylesheet' href='styles/site.css'>
-	</head>
+	<?php include('head.php'); ?>
 
 
 	<body>
